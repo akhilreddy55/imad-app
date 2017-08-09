747dfee44f253805dc218714e5e3268713ac5998
akhilreddy55 <30742186+akhilreddy55@users.noreply.github.com>
2017-08-09 18:57:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ function createTemplate(data){
     var heading=data.heading;
     var date=data.date;
     var content=data.content;
-    var htmlTemplate=`
+    var htmlTemplate={`
     <html>
         <head>
             <title>
@@ -58,7 +58,7 @@ function createTemplate(data){
             </div>
         </body>
     </html>
-    `
+    `};
     return htmlTemplate;
 }
 
