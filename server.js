24745a8f953700982c245f9bbb3c922ed660a8b1
Akhil Reddy <reddy.akhil65@gmail.com>
2017-08-09 18:47:58 +0530

    Made some changes locally in server.js file

--- server.js
+++ server.js
@@ -9,16 +9,62 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
-});
+var articles={
+  'artcile-one':{
+    title:'Article One | Akhil IMAD',
+    heading:'Article One',
+    date:'Aug 8,2017',
+    content:`<p> This is the content of my First Article.This is the content of my First Article.This is the content of my First Article.This is the content of my First Article.This is the content of my First Article.</p>
+    <p>This is the content of my First Article.This is the content of my First Article.This is the content of my First Article.This is the content of my First Article.This is the content of my First Article.</p>
+    <p>This is the content of my First Article.
+    </p>`},
+  'article-two':{
+    title:'Article Two | Akhil IMAD',
+    heading:'Article Two',
+    date:'Aug 18,2017',
+    content:`<p> This is the content of my Second Article.
+    </p>`
+  },
+  'article-three':{
+    title:'Article Three | Akhil IMAD',
+    heading:'Article Three',
+    date:'Aug 28,2017',
+    content:`<p> This is the content of my Third Article.
+    </p>`
+  }
+};
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    var htmlTemplate=`
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+           <div class='container'>
+                <a href='/'>Home</a>
+                <hr/>
+                <h1>${heading}</h1>
+                <div>${date}</div>
+                <div>
+                ${content}
+                </div>
+            </div>
+        </body>
+    </html>
+    `
+    return htmlTemplate;
+}
 
-app.get('/article-two', function (req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html')); 
-});
-
-app.get('/article-three', function (req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
+app.get('/:articleName', function (req, res){
+ articleName=req.params.articleName;
+   res.send(createTemplate(article[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
