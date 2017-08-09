0b77660b56c81a7a92a39dbe11035bc803e00e5f
akhilreddy55 <30742186+akhilreddy55@users.noreply.github.com>
2017-08-09 22:14:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 //change the text of main text div in html
 var element=document.getElementById('main-text');
 element.innerHTML = 'New Value';
+
+//moving image
+var img=document.getElementById('madi');
+img.onclick=function(){
+    img.style.marginLeft='100px';
+};
